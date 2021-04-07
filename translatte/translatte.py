add43e497df086d31ca27e4e5c743126c29418de
Weifeng Zhong <weifeng@weifengzhong.com>
2021-04-07 00:57:38 -0400

    added Amazon Translate engine

--- translatte/translatte.py
+++ translatte/translatte.py
@@ -5,6 +5,10 @@ from urllib.request import urlopen, Request
 from urllib.parse import quote
 import gzip
 import json
+import sys
+
+sys.path.append('../..')
+from src.aws_utils import connect_amazon_translate
 
 
 class Translator:
@@ -23,7 +27,16 @@ class Translator:
         return translation
 
     @staticmethod
-    def translate(input_text, target_lang, source_lang='auto'):
+    def translate_google(input_text, target_lang, source_lang='auto'):
         url = f"https://translate.googleapis.com/translate_a/single?client=gtx&sl={source_lang}&tl={target_lang}&dt=t&q={quote(input_text)}"
         response = Translator.get_response_from_request(Request(url=url))
         return Translator.get_translation_from_response(response)
+
+    @staticmethod
+    def translate_amazon(input_text, target_lang, source_lang='auto'):
+        translate = connect_amazon_translate()
+        try:
+            result = translate.translate_text(Text=input_text, SourceLanguageCode=source_lang, TargetLanguageCode=target_lang).get('TranslatedText')
+        except:
+            result = input_text
+        return result
